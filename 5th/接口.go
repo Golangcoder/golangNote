82f5c14ee4f84b19927bac60b444b70b9dfae3ed
Golangcoder@gmail.com <Golangcoder@gmail.com>
2014-10-25 22:55:52 +0800

    添加了一些接口的说明和代码示例

--- 5th/接口.go
+++ 5th/接口.go
@@ -25,11 +25,12 @@ func (self Phoneconnector) Test() {
 	fmt.Println("hello test")
 }
 func main() {
-	var a interface{}
-	fmt.Println(a == nil)
-	var p *int = nil
-	a = p
-	fmt.Println(a == nil)
+	var a USB
+	b := Phoneconnector{name: "Nokia"}
+	a = USB(b)
+	a.Connect()
+	b.name = "HTC"
+	a.Connect()
 }
 func Disconnect(usb interface{}) {
 	// if pc, ok := usb.(Phoneconnector); ok {
@@ -69,16 +70,18 @@ a = PhoneConnecter{name:"Nokia"}//初始化PhoneConnecter这个结构，并字�
 通过类型判定来调用这个属性。教程中给出了两种方法：
 第一种：
 	func Disconnect(usb USB){//定义了一个方法，传入了类型为USB的参数
-	if k,ok:=usb.(PhoneConnecter);ok{
+	if k,ok:=usb.(PhoneConnecter);ok{//判断传入的参数是不我们预设的类型PhoneConnecter?如果是就输出
 	fmt.Println(k.name,"Disconnected")
 	return
 	}
-	fmt.Println("Uknown Device")
+	fmt.Println("Uknown Device")//如果不是，打印未知设备
 	}
 第二种：
-	func Disconnecter(usb interface{}){
-	switch v:=usb.(type){
-	case Phoneconnecter:
+	func Disconnecter(usb interface{}){//这里传入了一个空接口，空接口中没有任何方法，根据go语言接口实现的设定，任何结构都
+		//实现了空接口，所以这里可以被传入的类型是很多的。
+	switch v:=usb.(type){//这里还是断定下传入参数的类型，不过这里我们不是判断他是不是我们预设的类型，而是让系统来判断，类型处
+		//填写了type。
+	case Phoneconnecter://用了switch-case来实现类型的判断，并输出相应内容。
 		fmt.Println(v.name,"Disconnected")
 	default:
 		fmt.Println("Uknown Device")
@@ -87,13 +90,29 @@ a = PhoneConnecter{name:"Nokia"}//初始化PhoneConnecter这个结构，并字�
 4.接口的转换：拥有方法超集的接口可以向拥有方法子集的接口进行转换，但逆向转换是不可以的。也就是说，接口转换遵循着从
 大到小的转换原则。
 5.接口对象和类型都为nil才为nil
+func main() {
+	var a interface{}
+	fmt.Println(a == nil)//这里a是一个空接口，没有赋值
+	var p *int = nil//这里p是一个指针，为空指针。
+	a = p//这里把这个空指针赋值给了空接口变量a（再次说明了，其他结构都实现了空接口这句话）
+	fmt.Println(a == nil)//这里接口变量a是一个为空的指针，虽然这个指针为nil，但根据对象和类型均为nil才为nil的原则。这里是false
+}
 6.对象赋值给接口时，是值的复制，接口内部存储的是指向这个复制品的指针。无法获取复制品的指针，也无法修改复制品的状态。
+func main() {
+	var a USB
+	b := Phoneconnector{name: "Nokia"}//初始化了一个结构Phoneconnecter
+	a = USB(b)//将其转换为USB类型(interface)
+	a.Connect()
+	b.name = "HTC"//这里我们更改了赋值源对象的属性，并再次调用接口a的方法，从结果中也可以看到，接口中存储的属性值name并没有被改变。
+	a.Connect()
+}
 7.指针和值的方法集问题，前面遇到了这样一种情况：
-var p *int 
+var p *int
 var a int = 10
 p = &a
 p++
 fmt.Println(*p)//11
 也就是说，传入一个指针，因为指针的方法集中包含了取指针对应值这样的方法，那么内部就有如同：(*p)++这样的存在，但是反过来却不成立。
-传入一个值，那么，他只有值本身的方法。不会进行隐式的调用转换。
+传入一个值，那么，他只有值本身的方法。不会进行隐式的调用转换。//这里是一个method set & call的内容，golang.org 的wiki中有说明
+会单独写一个东东说这块内容。
 */
